e9511a4aeab57c4c0c354312aed4de81d140f89b
AndsLog <leanjn0922@gmail.com>
2018-04-30 23:45:03 +0800

    add search law method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log("App start");
+console.log('App start');
 
 const line = require('@line/bot-sdk');
 const express = require('express');
@@ -10,16 +10,17 @@ const cheerio = require('cheerio');
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: 'YBj7tCC0v+3hSQS1OPOI3WGgpuiWwOqA+vs6AlZPgc7kksKaZnXXKMSDovk492L64WbEBSRPcq7Yr9WZ8EKS0un7/AJPpCP92Rr4Y+H+DxUnF/ZVzS20I+iNEBCe1oGEblvmazUj0D5157DnSQhAagdB04t89/1O/w1cDnyilFU=',
-  channelSecret: 'cb52a3777c92132d08710579a22a5424',
+  channelSecret: 'cb52a3777c92132d08710579a22a5424'
 };
 
 const lawGovUrl = 'https://lis.ly.gov.tw/lglawc/lglawkm';
-const cuntryLawUrl = 'https://law.moj.gov.tw//Schedule/cll.html';
+const countryLawUrl = 'https://law.moj.gov.tw//Schedule/cll.html';
 
+let userId = '';
 // create LINE SDK client
 const client = new line.Client(config);
 
-console.log("Line bot create");
+console.log('Line bot create');
 
 // create Express app
 // about Express itself: https://expressjs.com/
@@ -30,7 +31,6 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
-
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
@@ -45,21 +45,49 @@ app.post('/callback', line.middleware(config), (req, res) => {
 });
 
 // event handler
-function handleEvent(event) {
+function handleEvent (event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  // const echo = { type: 'text', text: event.message.text };
+  userId = event.source.userId;
+  lawSearch();
 
   // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  // return client.replyMessage(event.replyToken, echo);
 }
 
-function lawSearch() {
-  request.get(lawGovUrl, function(error) {
+function lawSearch () {
+  request({
+    url: lawGovUrl,
+    method: 'GET'
+  }, function (error, response, body) {
+    if (error || !body) {
+      return;
+    }
+    let $ = cheerio.load(body); // 載入 body
+    let lawsTableTrs = $('table.sumtab').find('tr');
+    let results = [];
+    for (let i = 1; i < lawsTableTrs.length; i++) {
+      let lawsTds = lawsTableTrs.eq(i).find('td');
+      let status = lawsTds.eq(2).text();
+      let content = lawsTds.eq(3).text();
 
+      let publishedLaw = {
+        type: 'text',
+        text: status.replace(/\r\n|\n/g, '') + ': ' + content.replace(/\r\n|\n/g, '')
+      };
+      results.push(publishedLaw);
+
+      if (results.length === 5) {
+        console.log(results);
+        client.pushMessage(userId, results);
+        results = [];
+        console.log('empty: ' + results);
+      }
+    }
   });
 }
