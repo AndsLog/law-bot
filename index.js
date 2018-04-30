bf165399d74ad0224a232376064875fedb45bebf
AndsLog <leanjn0922@gmail.com>
2018-05-01 02:34:06 +0800

    add date and url but url can't link

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 // create LINE SDK config from env variables
-const config = {
-  channelAccessToken: 'YBj7tCC0v+3hSQS1OPOI3WGgpuiWwOqA+vs6AlZPgc7kksKaZnXXKMSDovk492L64WbEBSRPcq7Yr9WZ8EKS0un7/AJPpCP92Rr4Y+H+DxUnF/ZVzS20I+iNEBCe1oGEblvmazUj0D5157DnSQhAagdB04t89/1O/w1cDnyilFU=',
-  channelSecret: 'cb52a3777c92132d08710579a22a5424'
+// const config = {
+//   channelAccessToken: 'YBj7tCC0v+3hSQS1OPOI3WGgpuiWwOqA+vs6AlZPgc7kksKaZnXXKMSDovk492L64WbEBSRPcq7Yr9WZ8EKS0un7/AJPpCP92Rr4Y+H+DxUnF/ZVzS20I+iNEBCe1oGEblvmazUj0D5157DnSQhAagdB04t89/1O/w1cDnyilFU=',
+//   channelSecret: 'cb52a3777c92132d08710579a22a5424'
+// };
+
+const config_cat = {
+  channelAccessToken: 'CHSFSSkScq10baLGKQO0AWVoZ6hTTA3x8xSbAiwF1TY4rNBDZqiPFXsgpgN9mycyQUBVcouOMaSOY83jZs0HiQaRZCUZgnn98r15EVDg40U/FFPGGyyRp/zHlSD3rZYZkCZRW7uEQHEufpW2YRG0+wdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '21b2b8ce572c642f88c051edd182999b'
 };
 
 const lawGovUrl = 'https://lis.ly.gov.tw/lglawc/lglawkm';
@@ -18,7 +23,7 @@ const countryLawUrl = 'https://law.moj.gov.tw//Schedule/cll.html';
 
 let userId = '';
 // create LINE SDK client
-const client = new line.Client(config);
+const client = new line.Client(config_cat);
 
 console.log('Line bot create');
 
@@ -34,7 +39,7 @@ app.listen(port, () => {
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
+app.post('/callback', line.middleware(config_cat), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.status(200).json(result))
@@ -73,12 +78,15 @@ function lawSearch () {
     let results = [];
     for (let i = 1; i < lawsTableTrs.length; i++) {
       let lawsTds = lawsTableTrs.eq(i).find('td');
+      let date = lawsTds.eq(1).text();
       let status = lawsTds.eq(2).text();
-      let content = lawsTds.eq(3).text();
+      let content = lawsTds.eq(3).find('a').text();
+      let url = lawsTds.eq(3).find('a').attr('href');
 
       let publishedLaw = {
         type: 'text',
-        text: status.replace(/\r\n|\n/g, '') + ': ' + content.replace(/\r\n|\n/g, '')
+        text: date.replace(/\r\n|\n/g, '') + '， ' + status.replace(/\r\n|\n/g, '') + ': ' + content.replace(/\r\n|\n/g, '') +
+        ' 網址: https://lis.ly.gov.tw' + url.toString()
       };
       results.push(publishedLaw);
 
